d1e3477ce4455024d10b0b40d900f387f6b32628
拒绝者 <asd1161575755@outlook.com>
2025-05-04 21:45:26 +0800

    refactor✨️: 重构搜索框和按钮样式     - 创建专门的 createSearchBox 和 createButton 方法来构建搜索框和按钮     - 添加焦点事件监听器以在获得和失去焦点时更新边框颜色     - 优化 MainPanel 类的结构，提高代码的可读性和可维护性 build: 🔨️️️️版本提升 0.7.17 -> 0.7.17.1

--- src/main/java/com/acme/json/helper/ui/panel/MainPanel.java
+++ src/main/java/com/acme/json/helper/ui/panel/MainPanel.java
@@ -29,10 +29,9 @@ import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -74,14 +73,11 @@ public class MainPanel {
         final JPanel searchPanel = new JPanel(new BorderLayout(0, 0));
         searchPanel.setBorder(BorderFactory.createEmptyBorder());
         // 搜索框
-        final JTextField searchBox = new JTextField();
-        searchBox.setMargin(JBUI.emptyInsets());
-        searchBox.setPreferredSize(new Dimension(220, 35));
-        searchBox.setToolTipText(BUNDLE.getString("json.tool.tip.text"));
+        final JTextField searchBox = this.createSearchBox();
         // 按钮组`撤消按钮`、`重做按钮`、`清除按钮`
-        final JButton undoButton = createButton(AllIcons.Actions.Undo);
-        final JButton redoButton = createButton(AllIcons.Actions.Redo);
-        final JButton clearButton = createButton(AllIcons.Actions.ClearCash);
+        final JButton undoButton = this.createButton(AllIcons.Actions.Undo, Boolean.TRUE, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE);
+        final JButton redoButton = this.createButton(AllIcons.Actions.Redo, Boolean.TRUE, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE);
+        final JButton clearButton = this.createButton(AllIcons.Actions.ClearCash, Boolean.TRUE, Boolean.FALSE, Boolean.TRUE, Boolean.TRUE);
         // 添加按钮面板
         final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
         buttonPanel.setBorder(BorderFactory.createEmptyBorder());
@@ -91,10 +87,39 @@ public class MainPanel {
         searchPanel.add(buttonPanel, BorderLayout.EAST);
         searchPanel.add(searchBox, BorderLayout.CENTER);
         // 编辑器动作
-        editorAction(searchBox, redoButton, undoButton, editor);
+        this.editorAction(searchBox, redoButton, undoButton, editor);
         // 编辑器监听
-        listener(editor, undoButton, redoButton, clearButton);
+        this.listener(editor, undoButton, redoButton, clearButton);
         return searchPanel;
+    }
+
+    /**
+     * 创建搜索框
+     *
+     * @return {@link JTextField }
+     */
+    private JTextField createSearchBox() {
+        final JTextField searchBox = new JTextField();
+        searchBox.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(final FocusEvent e) {
+                if (e.getSource() instanceof final JTextField field) {
+                    field.setBorder(MainPanel.this.createBorderByDefaultColor());
+                }
+            }
+
+            @Override
+            public void focusLost(final FocusEvent e) {
+                if (e.getSource() instanceof final JTextField field) {
+                    field.setBorder(MainPanel.this.createBorderByDefaultColor());
+                }
+            }
+        });
+        searchBox.setMargin(JBUI.emptyInsets());
+        searchBox.setBorder(MainPanel.this.createBorderByDefaultColor());
+        searchBox.setPreferredSize(new Dimension(220, 35));
+        searchBox.setToolTipText(BUNDLE.getString("json.tool.tip.text"));
+        return searchBox;
     }
 
     /**
@@ -102,13 +127,58 @@ public class MainPanel {
      * @param icon 按钮图标
      * @return 配置好的JButton实例
      */
-    private JButton createButton(final Icon icon) {
-        final JButton button = new JButton();
-        button.setIcon(icon);
-        button.setEnabled(Boolean.FALSE);
-        button.setMargin(JBUI.emptyInsets());
-        button.setPreferredSize(new Dimension(35, 35));
-        return button;
+    @SuppressWarnings("SameParameterValue")
+    private JButton createButton(final Icon icon, final boolean top, final boolean left, final boolean bottom, final boolean right) {
+        final JButton toolButton = new JButton();
+        toolButton.setIcon(icon);
+        toolButton.setEnabled(Boolean.FALSE);
+        toolButton.setMargin(JBUI.emptyInsets());
+        toolButton.setPreferredSize(new Dimension(35, 35));
+        toolButton.setBorder(MainPanel.this.createBorderByDefaultColor(top, left, bottom, right));
+        toolButton.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(final FocusEvent e) {
+                if (e.getSource() instanceof final JButton button) {
+                    button.setBorder(MainPanel.this.createBorderByDefaultColor(top, left, bottom, right));
+                }
+            }
+
+            @Override
+            public void focusLost(final FocusEvent e) {
+                if (e.getSource() instanceof final JButton button) {
+                    button.setBorder(MainPanel.this.createBorderByDefaultColor(top, left, bottom, right));
+                }
+            }
+        });
+        return toolButton;
+    }
+
+    /**
+     * 按默认颜色创建边框`四周边框`
+     *
+     * @return {@link Border }
+     */
+    private Border createBorderByDefaultColor() {
+        return BorderFactory.createLineBorder(UIManager.getColor("Component.borderColor"), 1);
+    }
+
+    /**
+     * 按默认颜色创建边框`自定义边框`
+     *
+     * @param top 顶端
+     * @param left 左边
+     * @param bottom 底部
+     * @param right 正确
+     * @return {@link Border }
+     */
+    private Border createBorderByDefaultColor(final boolean top, final boolean left, final boolean bottom, final boolean right) {
+        return BorderFactory.createMatteBorder(
+                top ? 1 : 0,
+                left ? 1 : 0,
+                bottom ? 1 : 0,
+                right ? 1 : 0,
+                UIManager.getColor("Component.borderColor")
+        );
     }
 
     /**
@@ -117,8 +187,8 @@ public class MainPanel {
      * @param redoButton 重做按钮
      */
     private void updateButtons(final JButton undoButton, final JButton redoButton) {
-        undoButton.setEnabled(!undoStack.isEmpty());
-        redoButton.setEnabled(!redoStack.isEmpty());
+        undoButton.setEnabled(!this.undoStack.isEmpty());
+        redoButton.setEnabled(!this.redoStack.isEmpty());
     }
 
     /**
@@ -128,13 +198,13 @@ public class MainPanel {
      * @param editor     当前编辑
      */
     private void redoLastSearch(final JButton redoButton, final JButton undoButton, final EditorTextField editor) {
-        if (Objects.isNull(editor) || redoStack.isEmpty()) return;
+        if (Objects.isNull(editor) || this.redoStack.isEmpty()) return;
         // 储存撤销历史
-        undoStack.push(editor.getDocument().getText());
+        this.undoStack.push(editor.getDocument().getText());
         // 将重做历史写回编辑器
-        editor.setText(redoStack.pop());
+        editor.setText(this.redoStack.pop());
         // 更新按钮可用状态
-        updateButtons(undoButton, redoButton);
+        this.updateButtons(undoButton, redoButton);
     }
 
     /**
@@ -144,16 +214,16 @@ public class MainPanel {
      * @param editor     当前编辑
      */
     private void undoLastSearch(final JButton redoButton, final JButton undoButton, final EditorTextField editor) {
-        if (Objects.isNull(editor) || undoStack.isEmpty()) {
-            originalJson.set("");
+        if (Objects.isNull(editor) || this.undoStack.isEmpty()) {
+            this.originalJson.set("");
             return;
         }
         // 储存重做历史
-        redoStack.push(editor.getDocument().getText());
+        this.redoStack.push(editor.getDocument().getText());
         // 将撤消历史写回编辑器
-        editor.setText(undoStack.pop());
+        editor.setText(this.undoStack.pop());
         // 更新按钮可用状态
-        updateButtons(undoButton, redoButton);
+        this.updateButtons(undoButton, redoButton);
     }
 
     /**
@@ -163,15 +233,15 @@ public class MainPanel {
     private void clearContent(final JButton redoButton, final JButton undoButton, final EditorTextField editor) {
         if (Objects.isNull(editor)) return;
         // 储存撤消历史
-        undoStack.push(editor.getDocument().getText());
+        this.undoStack.push(editor.getDocument().getText());
         // 清空原始记录
-        originalJson.set("");
+        this.originalJson.set("");
         // 清空重做历史
-        redoStack.clear();
+        this.redoStack.clear();
         // 清空编辑器
         editor.setText("");
         // 更新按钮可用状态
-        updateButtons(undoButton, redoButton);
+        this.updateButtons(undoButton, redoButton);
     }
 
     /**
@@ -187,13 +257,13 @@ public class MainPanel {
         final Document document = editor.getDocument();
         if (document.getText().isEmpty()) return;
         // 储存原始记录
-        originalJson.compareAndSet("", document.getText());
+        this.originalJson.compareAndSet("", document.getText());
         // 储存撤销历史
-        undoStack.push(document.getText());
+        this.undoStack.push(document.getText());
         // 搜索结果写回编辑器
-        editor.setText(new JsonSearchEngine().process(originalJson.get(), searchExpression));
+        editor.setText(new JsonSearchEngine().process(this.originalJson.get(), searchExpression));
         // 更新按钮可用状态
-        updateButtons(undoButton, redoButton);
+        this.updateButtons(undoButton, redoButton);
     }
 
     /**
@@ -207,28 +277,28 @@ public class MainPanel {
                                      final EditorTextField editor, final MouseEvent e) {
         final DefaultActionGroup group = new DefaultActionGroup();
         // 格式化菜单
-        addJsonAction(group, "json.format.json", "json.format.json.desc",
+        this.addJsonAction(group, "json.format.json", "json.format.json.desc",
                 AllIcons.Actions.Refresh, new JsonFormatter(), redoButton, undoButton, editor);
         // 压缩菜单
-        addJsonAction(group, "json.compress.json", "json.compress.json.desc",
+        this.addJsonAction(group, "json.compress.json", "json.compress.json.desc",
                 AllIcons.Actions.Collapseall, new JsonCompressor(), redoButton, undoButton, editor);
         // 转义菜单
-        addJsonAction(group, "json.escaping.json", "json.escaping.json.desc",
+        this.addJsonAction(group, "json.escaping.json", "json.escaping.json.desc",
                 AllIcons.Javaee.UpdateRunningApplication, new JsonEscaper(), redoButton, undoButton, editor);
         // 去转义菜单
-        addJsonAction(group, "json.un.escaping.json", "json.un.escaping.json.desc",
+        this.addJsonAction(group, "json.un.escaping.json", "json.un.escaping.json.desc",
                 AllIcons.Actions.SearchNewLine, new JsonUnEscaper(), redoButton, undoButton, editor);
         // 修复菜单
-        addJsonAction(group, "json.repair.json", "json.repair.json.desc",
+        this.addJsonAction(group, "json.repair.json", "json.repair.json.desc",
                 AllIcons.Toolwindows.ToolWindowBuild, new JsonRepairer(), redoButton, undoButton, editor);
         // 分隔符
         group.addSeparator();
         // 差异对比菜单
-        addDiffAction(group, editor);
+        this.addDiffAction(group, editor);
         // 转为任何
-        addJsonToAnyAction(group, editor);
+        this.addJsonToAnyAction(group, editor);
         // 添加打开文件菜单
-        addOpenFileAction(group, editor);
+        this.addOpenFileAction(group, editor);
         // 分隔符
         group.addSeparator();
         // 其他可适配的菜单
@@ -254,7 +324,7 @@ public class MainPanel {
         group.add(new AnAction(BUNDLE.getString(nameKey), BUNDLE.getString(descKey), icon) {
             @Override
             public void actionPerformed(final @NotNull AnActionEvent e) {
-                optJson(redoButton, undoButton, editor.getEditor(), operation);
+                MainPanel.this.optJson(redoButton, undoButton, editor.getEditor(), operation);
             }
         });
     }
@@ -289,9 +359,9 @@ public class MainPanel {
      */
     private void listener(final EditorTextField editor, final JButton undoButton, final JButton redoButton, final JButton clearButton) {
         // 编辑器事件
-        clearButton.addActionListener(e -> clearContent(redoButton, undoButton, editor));
-        undoButton.addActionListener(e -> undoLastSearch(redoButton, undoButton, editor));
-        redoButton.addActionListener(e -> redoLastSearch(redoButton, undoButton, editor));
+        clearButton.addActionListener(e -> this.clearContent(redoButton, undoButton, editor));
+        undoButton.addActionListener(e -> this.undoLastSearch(redoButton, undoButton, editor));
+        redoButton.addActionListener(e -> this.redoLastSearch(redoButton, undoButton, editor));
         // 激活清空按钮
         editor.getDocument().addDocumentListener(new DocumentListener() {
             @Override
@@ -303,14 +373,14 @@ public class MainPanel {
                 // 根据文档内容调整清空按钮的状态
                 clearButton.setEnabled(!StrUtil.isEmpty(text));
                 // 自动识别`web路径`或`本地文件路径`转为JSON，写回编辑器
-                OptPath(text, editor, e, this);
+                MainPanel.this.OptPath(text, editor, e, this);
             }
         });
         // 上下文菜单
         editor.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(final MouseEvent e) {
-                if (e.isPopupTrigger()) showEditorPopupMenu(redoButton, undoButton, editor, e);
+                if (e.isPopupTrigger()) MainPanel.this.showEditorPopupMenu(redoButton, undoButton, editor, e);
             }
         });
     }
@@ -326,7 +396,7 @@ public class MainPanel {
         new AnAction() {
             @Override
             public void actionPerformed(final @NotNull AnActionEvent e) {
-                optJson(redoButton, undoButton, editor.getEditor(), new JsonFormatter());
+                MainPanel.this.optJson(redoButton, undoButton, editor.getEditor(), new JsonFormatter());
             }
         }.registerCustomShortcutSet(
                 new CustomShortcutSet(KeymapManager.getInstance().getActiveKeymap().getShortcuts("ReformatCode")),
@@ -336,7 +406,7 @@ public class MainPanel {
         new AnAction() {
             @Override
             public void actionPerformed(final @NotNull AnActionEvent e) {
-                performSearch(searchField, redoButton, undoButton, editor);
+                MainPanel.this.performSearch(searchField, redoButton, undoButton, editor);
             }
         }.registerCustomShortcutSet(
                 new CustomShortcutSet(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0)),
@@ -364,7 +434,7 @@ public class MainPanel {
                     if (JSON.isValid(processedText)) {
                         try {
                             e.getDocument().removeDocumentListener(listener);
-                            originalJson.set("");
+                            this.originalJson.set("");
                             editor.setText(new JsonFormatter().process(processedText));
                         } finally {
                             e.getDocument().addDocumentListener(listener);
@@ -387,11 +457,11 @@ public class MainPanel {
         if (!operation.isValid(document.getText())) return;
         WriteCommandAction.runWriteCommandAction(editor.getProject(), () -> {
             // 储存撤销历史
-            undoStack.push(document.getText());
+            this.undoStack.push(document.getText());
             // 将操作结果写回编辑器
             document.setText(StringUtil.convertLineSeparators(operation.process(document.getText())));
             // 更新按钮可用状态
-            updateButtons(undoButton, redoButton);
+            this.updateButtons(undoButton, redoButton);
         });
     }
 
@@ -408,7 +478,7 @@ public class MainPanel {
         ) {
             @Override
             public void actionPerformed(@NotNull final AnActionEvent e) {
-                handleFileOpen(editor);
+                MainPanel.this.handleFileOpen(editor);
             }
         });
     }
@@ -426,7 +496,7 @@ public class MainPanel {
         ) {
             @Override
             public void actionPerformed(@NotNull final AnActionEvent e) {
-                showDiffViewer(editor);
+                MainPanel.this.showDiffViewer(editor);
             }
         });
     }
@@ -451,10 +521,10 @@ public class MainPanel {
                                     // 将文本写入编辑器
                                     editor.getDocument().setText(text);
                                     // 储存原始记录
-                                    originalJson.set(text);
+                                    this.originalJson.set(text);
                                     // 清空撤销、重做记录
-                                    undoStack.clear();
-                                    redoStack.clear();
+                                    this.undoStack.clear();
+                                    this.redoStack.clear();
                                 });
                                 Notifier.notifyInfo("%s%s".formatted(BUNDLE.getString("file.load.success"), virtualFile.getPath()), editor.getProject());
                             });
